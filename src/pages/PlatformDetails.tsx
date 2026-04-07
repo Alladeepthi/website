e2487ca586d2f3f660499d829b442f03002dcbd8
alla deepthi <alladeepthi451@gmail.com>
2026-04-07 15:03:38 +0530

    fix(build): remove unused useState import and verified production build

--- src/pages/PlatformDetails.tsx
+++ src/pages/PlatformDetails.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useParams, Navigate, Link } from 'react-router-dom';
 import { platformsData } from '../data/platformsData';
 
